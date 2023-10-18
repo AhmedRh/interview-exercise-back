e1542b87bfe1914e33ddba0506440c554138e916
AhmedRh <benrehayem.ahmed@gmail.com>
2023-10-18 13:48:05 +0200

    init repo

---
+++ src/main/java/com/neosoft/interviewback/service/NeoSoFtService.java
@@ -0,0 +1,14 @@
+package com.neosoft.interviewback.service;
+
+import org.springframework.stereotype.Service;
+
+import static java.lang.String.valueOf;
+import static java.util.Objects.isNull;
+
+@Service
+public class NeoSoFtService {
+
+    public String convertNumber(int inputNumber) {
+        return "";
+    }
+}
